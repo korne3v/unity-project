33ed2f475d412e5f26024d66b7b04596d8cd39c3
korne3v <50340775+korne3v@users.noreply.github.com>
2020-09-21 13:00:21 +0300

    Add files via upload

---
+++ Entity.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Entity : MonoBehaviour
+{
+
+    public int health = 100;
+    public Image damageBackground;
+
+    void Update()
+    {
+        if (health < 0) {
+            health = 0;
+        }
+
+        Color color = damageBackground.color;
+        color.a -= Time.deltaTime;
+        damageBackground.color = color;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void damage(int damage, DamageCause cause) {
+
+        this.health -= damage;
+
+        if (cause == DamageCause.FALL)
+        {
+            Debug.Log("Вы упали и потеряли " + damage + " здоровья");
+            Color color = damageBackground.color;
+            color.a = 0.3F;
+            damageBackground.color = color;
+        }
+        else {
+            Color color = damageBackground.color;
+            color.a = 0.2F;
+            damageBackground.color = color;
+        }
+
+    }
+
+    public enum DamageCause {
+
+        PLAYER_ATTACK,
+        FALL,
+        FIRE,
+        OTHER
+
+    }
+
+}
---
+++ Movement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public Entity entity;
+
+    public float walkSpeed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float runSpeed = 8.0F;
+    public float gravity = 20.0F;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    public float fallDistance = 0;
+    public float fallY = 0;
+
+    void Update()
+    {
+
+        if (controller.isGrounded)
+        {
+
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+
+            moveDirection *= Input.GetKey(KeyCode.LeftShift) ? walkSpeed / 3 : walkSpeed;
+
+            if (fallDistance > 10)
+            {
+                int damage = (int)fallDistance;
+                Debug.Log("You getting damage in " + damage);
+                entity.damage(damage, Entity.DamageCause.FALL);
+            }
+
+            fallDistance = 0;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+                fallDistance = -8;
+            }
+
+        }
+        else {
+            fallDistance += gravity * Time.deltaTime;
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+
+    }
+
+    public void setFallDistance(int value) {
+        this.fallDistance = value;
+    }
+
+}
